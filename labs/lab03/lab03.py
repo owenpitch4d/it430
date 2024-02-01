62270dadd11a4fdea62c9492532f26c32504dba9
owenpitch4d <m255076@usna.edu>
2024-02-01 10:24:32 -0500

    lab03

--- labs/lab03/lab03.py
+++ labs/lab03/lab03.py
@@ -1,19 +1,78 @@
 import struct
 def showpkts(data):
-    data = data[36:] #remove global header
+    data = data[24:] #remove global header
     
-    pkt_len = int.from_bytes(data[0:4], "little")
-    data = data[4:]
     while(data):
-        hex_column1 = " ".join([f"{i:02x}" for i in data[0:8]])
-        hex_column2 = " ".join([f"{i:02x}" for i in data[8:16]])
-        hex_column = hex_column1 + "  " + hex_column2
-        print(f"{hex_column:<49}")
-        data = data[16:]
+        #remove excess header but leave the size port
+        data = data[12:]
+        print("data:")
 
-            
+        #find packet length
+        pkt_len = int.from_bytes(data[0:4], "little")
+        
+        data = data[4:] #get rid of length
 
+        #convert only the first packet to hex
+        tempData = ([f"{i:02x}" for i in data[0:pkt_len]])
+        
+        for index, element in enumerate(tempData, start=1):
+            print(element, end='')
+            if index%16==0:
+                print("")
+            elif index%8==0:
+                print(" ", end='')
+            else:
+                print(" ", end='')
+        data = data[pkt_len:]
+        print("\n")
+    
 
+    #data = open("two.pcap", "rb").        data = data[12:]read()
+    #showpkts(data)
+        
+def showpkts_Eth(data):
+    data = data[24:] #remove global header
+    
+    while(data):
+        #remove excess header but leave the size port
+        data = data[12:]
+        print("data:")
 
-data = open("two.pcap", "rb").read()
-showpkts(data)
+        #find packet length
+        pkt_len = int.from_bytes(data[0:4], "little")
+        
+        data = data[4:] #get rid of length
+
+        #convert only the first packet to hex
+        tempData = ([f"{i:02x}" for i in data[0:pkt_len]])
+
+        print("Dst-MAC=", getAdr(tempData))
+        tempData = tempData[6:]
+
+        print("Src-MAC=", getAdr(tempData))
+        tempData = tempData[6:]
+
+        print("Type=", tempData[0] + " " + tempData[1])
+        tempData = tempData[2:]
+
+        for index, element in enumerate(tempData, start=1):
+            print(element, end='')
+            if index%16==0:
+                print("")
+            elif index%8==0:
+                print(" ", end='')
+            else:
+                print(" ", end='')
+        data = data[pkt_len:]
+        print("\n")
+    
+def getAdr(data2):
+    dstMac = ""
+    for index, element in enumerate(data[0:6], start = 1):
+        dstMac += element
+        if(index != 6):
+            dstMac += ":"
+    
+    return dstMac
+    #data = open("two.pcap", "rb").        data = data[12:]read()
+    #showpkts(data)
