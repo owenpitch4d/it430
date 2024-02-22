b410353daa7e53a766a4d0a7b9bc236ae4f0a8ad
owenpitch4d <m255076@usna.edu>
2024-02-22 10:23:46 -0500

    p01

---
+++ labs/lab06/p01.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# udp_spoof.py
+from socket import *
+import struct
+
+raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)
+
+udp_payload = b"alex and owen are awesome"
+udp_header = struct.pack(">HHHH", 255076, 9000, 8 + len(udp_payload), 0) #my alpha
+ip_payload = udp_header + udp_payload
+
+#ip_header
+version = 4
+ihl = 5
+
+version_ihl = (version << 4) | ihl
+type_of_service = 0
+total_length = 20 + len(ip_payload)
+ip_header = struct.pack(">BBH", version_ihl, type_of_service, total_length)
+ip_header += struct.pack(">HH", 12345, 0)
+ttl = 20
+protocol = 17
+checksum = 0
+ip_header += struct.pack(">BBH", ttl, protocol, checksum)
+
+ip_header += struct.pack(">BBBB", 6, 17, 11, 26) #our birthdays
+ip_header += struct.pack(">BBBB", 192, 168, 172, 4) #it430a
+ip_pkt = ip_header + ip_payload
+
+addr = ("192.168.172.4", 9000)
+raw_socket.sendto(ip_pkt, addr)
